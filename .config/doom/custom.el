477f4912acc1f3fe8a0efd9b898a8248a4be24a7
Jed <jedace07@proton.me>
2025-08-20 19:40:02 -0400

    the sharty vvon, btw

---
+++ .config/doom/custom.el
@@ -0,0 +1,36 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elfeed-feeds
+   '("https://lifehacker.com/feed/rss" "https://rss.xcancel.com/gnukeith/rss"
+     "https://rss.xcancel.com/fnaffangamenews/rss"
+     "http://rss.slashdot.org/Slashdot/slashdotMainatom"
+     "https://www.theverge.com/rss/index.xml"
+     "https://archlinux.org/feeds/news/"
+     "https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml"
+     "https://itsfoss.com/rss/" "https://matrix.org/atom.xml"
+     "https://www.phoronix.com/rss.php"
+     "https://www.youtube.com/feeds/videos.xml?channel_id=UChIs72whgZI9w6d6FhwGGHA"
+     "https://www.youtube.com/feeds/videos.xml?channel_id=UCFP49fL_BM6pE_EVggIe_OA"
+     "https://www.youtube.com/feeds/videos.xml?channel_id=UCONH73CdRXUjlh3-DdLGCPw"
+     "https://www.youtube.com/feeds/videos.xml?channel_id=UCwHwDuNd9lCdA7chyyquDXw"
+     "https://www.youtube.com/feeds/videos.xml?channel_id=UC7YOGHUfC1Tb6E4pudI9STA"
+     "https://www.youtube.com/feeds/videos.xml?channel_id=UCpFFItkfZz1qz5PpHpqzYBw"
+     "https://www.youtube.com/feeds/videos.xml?channel_id=UC6cXXSd9InsR1kaH-JHJeNw"
+     "https://www.youtube.com/feeds/videos.xml?channel_id=UCr9E7Yo606ns3ZsAW9RXtOg"
+     "https://www.youtube.com/feeds/videos.xml?channel_id=UCjoMDdReIZ6W1wC2IKPzePA"
+     "https://www.youtube.com/feeds/videos.xml?channel_id=UCGAmpBtWveYp0-3v9npnhvQ"
+     "https://www.youtube.com/feeds/videos.xml?channel_id=UCgGw5WSITWvDISNjydXPW3A"
+     "https://tilvids.com/feeds/videos.xml?videoChannelId=47"
+     "https://videos.lukesmith.xyz/feeds/videos.xml?videoChannelId=2"
+     "https://www.youtube.com/feeds/videos.xml?channel_id=UC4JX40jDee_tINbkjycV4Sg"))
+ '(package-selected-packages '(systemd vterm)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
