d16ad129c2846a0c14a76f5a437d1244d0e381b3
Jed <jedace07@proton.me>
2025-08-21 20:48:21 -0400

    n

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -4,8 +4,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+;;(require elfeed-goodies)
+;;(elfeed-goodies/setup)
  '(elfeed-feeds
-   '("https://lifehacker.com/feed/rss" "https://rss.xcancel.com/gnukeith/rss"
+   '("https://www.zdnet.com/sitemaps/news.xml" "https://www.omgubuntu.co.uk/feed"
+     "https://www.gamingonlinux.com/article_rss.php"
+     "https://www.youtube.com/feeds/videos.xml?channel_id=UC5UAwBUum7CPN5buc-_N1Fw"
+     "https://www.youtube.com/feeds/videos.xml?channel_id=UCnM02drQP-dF7WMgtJHR4Xw"
+     "https://lifehacker.com/feed/rss" "https://rss.xcancel.com/gnukeith/rss"
      "https://rss.xcancel.com/fnaffangamenews/rss"
      "http://rss.slashdot.org/Slashdot/slashdotMainatom"
      "https://www.theverge.com/rss/index.xml"
@@ -15,18 +21,13 @@
      "https://www.phoronix.com/rss.php"
      "https://www.youtube.com/feeds/videos.xml?channel_id=UChIs72whgZI9w6d6FhwGGHA"
      "https://www.youtube.com/feeds/videos.xml?channel_id=UCFP49fL_BM6pE_EVggIe_OA"
-     "https://www.youtube.com/feeds/videos.xml?channel_id=UCONH73CdRXUjlh3-DdLGCPw"
-     "https://www.youtube.com/feeds/videos.xml?channel_id=UCwHwDuNd9lCdA7chyyquDXw"
      "https://www.youtube.com/feeds/videos.xml?channel_id=UC7YOGHUfC1Tb6E4pudI9STA"
      "https://www.youtube.com/feeds/videos.xml?channel_id=UCpFFItkfZz1qz5PpHpqzYBw"
      "https://www.youtube.com/feeds/videos.xml?channel_id=UC6cXXSd9InsR1kaH-JHJeNw"
      "https://www.youtube.com/feeds/videos.xml?channel_id=UCr9E7Yo606ns3ZsAW9RXtOg"
-     "https://www.youtube.com/feeds/videos.xml?channel_id=UCjoMDdReIZ6W1wC2IKPzePA"
      "https://www.youtube.com/feeds/videos.xml?channel_id=UCGAmpBtWveYp0-3v9npnhvQ"
      "https://www.youtube.com/feeds/videos.xml?channel_id=UCgGw5WSITWvDISNjydXPW3A"
-     "https://tilvids.com/feeds/videos.xml?videoChannelId=47"
-     "https://videos.lukesmith.xyz/feeds/videos.xml?videoChannelId=2"
-     "https://www.youtube.com/feeds/videos.xml?channel_id=UC4JX40jDee_tINbkjycV4Sg"))
+     "https://videos.lukesmith.xyz/feeds/videos.xml?videoChannelId=2"))
  '(package-selected-packages '(systemd vterm)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
