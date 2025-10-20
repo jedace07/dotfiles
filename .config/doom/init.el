ca53568db3132c802f7b91e14354cd0f4e7ff02b
Jed <jedace07@proton.me>
2025-10-20 19:50:00 -0400

    asddsag

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -158,7 +158,11 @@
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
        ;;purescript        ; javascript, but functional
+<<<<<<< HEAD
        (python +lsp +pyright)            ; beautiful is better than ugly
+=======
+       (python +lsp +pyright)           ; beautiful is better than ugly
+>>>>>>> b6ce818 (asddsag)
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
