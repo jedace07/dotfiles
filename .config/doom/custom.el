c275ebc555574312da95a3f2f9c405e30cd65c9f
Jed <jedace07@proton.me>
2026-02-26 09:40:23 -0400

    Update image-dired viewer and refresh Emacs
    Updated image-dired-external-viewer to use ImageLounge via Flatpak.
    Updated the Emacs subproject to version
    85ccc61ddc2ac7a2206159f51d75b78e87957c70.

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -4,6 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(image-dired-external-viewer "/var/lib/flatpak/exports/bin/org.nomacs.ImageLounge")
  '(package-selected-packages '(systemd vterm))
  '(pdf-misc-print-program-executable "/usr/bin/lp")
  '(transmission-refresh-modes
