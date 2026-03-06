d75e6bdf492814013ccbcebd6542a73a34c27a84
Jed <jedace07@proton.me>
2026-03-06 13:18:25 -0400

    Update image and PDF handling configurations
    Added external image viewer and converter settings for image-dired and
    image operations. Updated transmission refresh modes to include peers
    mode. Updated the Emacs subproject to a new commit with revised
    configuration.

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -4,11 +4,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(image-dired-external-viewer "/var/lib/flatpak/exports/bin/org.nomacs.ImageLounge")
+ '(image-use-external-converter t)
  '(package-selected-packages '(systemd vterm))
  '(pdf-misc-print-program-executable "/usr/bin/lp")
  '(transmission-refresh-modes
    '(transmission-mode transmission-files-mode transmission-info-mode
-     transmission-peers-mode))
+                       transmission-peers-mode))
  '(vlf-application 'dont-ask))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
